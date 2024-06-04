de3821343386231ec367b950d59262a84507bd2c
EvanStefan <steve@actlife.gr>
2024-06-04 15:16:43 +0300

    Move grid to utils

--- src/mnist.py
+++ src/mnist.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from trial import Trial
+import utils
+import trial
 import time
 
 # test case for the mnist dataset #
@@ -36,28 +37,10 @@ featuresTest = scaler.transform(featuresTest)
 # test #
 
 
-grid = [
-  {
-    'C':[1e-4, 1e-2, 1, 1e2, 1e4],
-    'kernel':['linear'],
-    'degree':[1],
-    'gamma':[1],
-  },
-  {
-    'C':[1],
-    'kernel':['poly'],
-    'degree':[2, 3, 4],
-    'gamma':[1],
-  },
-  {
-    'C':[1],
-    'kernel':['rbf'],
-    'degree':[1],
-    'gamma':[1e-4, 1e-2, 1, 1e2, 1e4],
-  }
-]
-
-trial = Trial(DATANAME, featuresTrain, targetTrain, featuresTest, targetTest, grid)
+trial = trial.Trial(
+  DATANAME, featuresTrain, targetTrain, 
+  featuresTest, targetTest, utils.createGrid(),
+)
 stats = trial.trials()
 pd.DataFrame(stats, index=pd.RangeIndex(start=1, stop=len(stats)+1, name='Trial')).to_csv(LOGPATH, mode='w')
 print('Running time: ', time.time()-start)
